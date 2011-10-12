7a639e0c030da1bebdaffa958dfb01a002de1aa9
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-10-12 16:15:42 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'sqlite3'
+
+group :test do
+  gem 'cucumber'
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'capybara-webkit'
+  gem 'capybara-screenshot'
+  gem 'rspec'
+end
+
+group :development, :test do
+  gem 'ruby-debug19'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapybaraScreenshotTest::Application.config.secret_token = '958fe1f3975d8a5d30e8ceb71a3e840716677170b503723508a8c42f2a7fae9a330e53eb10a9edfcfd8f8b23988be612cd6ba6313444660a2d5376c38e9348bd'
